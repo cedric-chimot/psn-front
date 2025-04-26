52a54562936dea0cd1ef385ab6394c9c1822b0aa
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-26 14:42:08 +0200

    ajout models année et stats niveaux

---
+++ src/app/models/Annee.ts
@@ -0,0 +1,9 @@
+export class Annee {
+  id: number;
+  annee: number;
+
+  constructor(id: number, annee: number) {
+    this.id = id;
+    this.annee = annee;
+  }
+}
---
+++ src/app/models/StatsNiveaux.ts
@@ -0,0 +1,11 @@
+export class StatsNiveaux {
+  id: number;
+  niveau: number;
+  idAnnee: number;
+
+  constructor(id: number, niveau: number, idAnnee: number) {
+    this.id = id;
+    this.niveau = niveau;
+    this.idAnnee = idAnnee;
+  }
+}
