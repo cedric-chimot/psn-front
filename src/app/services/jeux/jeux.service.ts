416ae2dbb5a11a30bea0ad198211655ea142a1d4
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-28 13:04:11 +0200

    ajout des méthodes aux services jeux et plateforme

--- src/app/services/jeux/jeux.service.ts
+++ src/app/services/jeux/jeux.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Jeux } from '../../models/Jeux';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JeuxService {
+  private apiUrl = 'http://localhost:8080/api/jeux';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createJeu(jeu: Jeux): Observable<Jeux> {
+    return this.http.post<Jeux>(this.apiUrl, jeu);
+  }
+
+  getAllJeux(): Observable<Jeux[]> {
+    return this.http.get<Jeux[]>(`${this.apiUrl}/all`);
+  }
+
+  getJeuById(id: number): Observable<Jeux> {
+    return this.http.get<Jeux>(`${this.apiUrl}/${id}`);
+  }
+
+  updateJeu(jeu: Jeux): Observable<Jeux> {
+    return this.http.put<Jeux>(`${this.apiUrl}/update`, jeu);
+  }
+
+  deleteJeuById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  deleteAllJeux(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
--- src/app/services/plateforme/plateforme.service.ts
+++ src/app/services/plateforme/plateforme.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Plateforme } from '../../models/Plateforme';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlateformeService {
+private apiUrl = 'http://localhost:8080/api/plateforme';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createJeu(plateforme: Plateforme): Observable<Plateforme> {
+    return this.http.post<Plateforme>(this.apiUrl, plateforme);
+  }
+
+  getAllJeux(): Observable<Plateforme[]> {
+    return this.http.get<Plateforme[]>(`${this.apiUrl}/all`);
+  }
+
+  getJeuById(id: number): Observable<Plateforme> {
+    return this.http.get<Plateforme>(`${this.apiUrl}/${id}`);
+  }
+
+  updateJeu(plateforme: Plateforme): Observable<Plateforme> {
+    return this.http.put<Plateforme>(`${this.apiUrl}/update`, plateforme);
+  }
+
+  deleteJeuById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  deleteAllJeux(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
