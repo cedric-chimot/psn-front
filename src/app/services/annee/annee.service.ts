838ebcc80daa0b473f9f4e5f4cd196b01bc5746d
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-28 13:09:47 +0200

    ajout des méthodes au service années

--- src/app/services/annee/annee.service.ts
+++ src/app/services/annee/annee.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Annee } from '../../models/Annee';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnneeService {
+  private apiUrl = 'http://localhost:8080/api/annee';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Ajouter une année
+  createAnnee(annee: Annee): Observable<Annee> {
+    return this.http.post<Annee>(this.apiUrl, annee);
+  }
+
+  // Récupérer toutes les années
+  getAllAnnees(): Observable<Annee[]> {
+    return this.http.get<Annee[]>(`${this.apiUrl}/all`);
+  }
+
+  // Récupérer une année par son ID
+  getAnneeById(id: number): Observable<Annee> {
+    return this.http.get<Annee>(`${this.apiUrl}/${id}`);
+  }
+
+  // Mettre à jour une année
+  updateAnnee(annee: Annee): Observable<Annee> {
+    return this.http.put<Annee>(`${this.apiUrl}/update`, annee);
+  }
+
+  // Supprimer une année par son ID
+  deleteAnneeById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  // Supprimer toutes les années
+  deleteAllAnnees(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
