273959ae3ead60634b4032f9f3d410cdd6868aac
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-29 10:13:28 +0200

    ajout des méthodes au service stats niveaux

--- src/app/services/statsNiveaux/stats-niveaux.service.ts
+++ src/app/services/statsNiveaux/stats-niveaux.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { StatsNiveaux } from '../../models/StatsNiveaux';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsNiveauxService {
+private apiUrl = 'http://localhost:8080/api/statsNiveaux';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Ajouter une statistique de niveaux
+  createStatsNiveaux(statsNiveaux: StatsNiveaux): Observable<StatsNiveaux> {
+    return this.http.post<StatsNiveaux>(this.apiUrl, statsNiveaux);
+  }
+
+  // Récupérer toutes les statistiques de niveaux
+  getAllStatsNiveaux(): Observable<StatsNiveaux[]> {
+    return this.http.get<StatsNiveaux[]>(`${this.apiUrl}/all`);
+  }
+
+  // Récupérer une statistique de niveaux par son ID
+  getStatsNiveauxById(id: number): Observable<StatsNiveaux> {
+    return this.http.get<StatsNiveaux>(`${this.apiUrl}/${id}`);
+  }
+
+  // Mettre à jour une statistique de niveaux
+  updateStatsNiveaux(statsNiveaux: StatsNiveaux): Observable<StatsNiveaux> {
+    return this.http.put<StatsNiveaux>(`${this.apiUrl}/update`, statsNiveaux);
+  }
+
+  // Supprimer une statistique de niveaux par son ID
+  deleteStatsNiveauxById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  // Supprimer toutes les statistiques de niveaux
+  deleteAllStatsNiveaux(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
