60c6bb727a7a567dbae39e9ccbf61abedb1fe5b3
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-29 10:16:48 +0200

    ajout des méthodes au service stats trophées

--- src/app/services/statsTrophees/stats-trophees.service.ts
+++ src/app/services/statsTrophees/stats-trophees.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StatsTrophees } from '../../models/StatsTrophees';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsTropheesService {
+  private apiUrl = 'http://localhost:8080/api/statsTrophees';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Ajouter une statistique de statistique de trophées
+  createStatsTrophees(statsTrophees: StatsTrophees): Observable<StatsTrophees> {
+    return this.http.post<StatsTrophees>(this.apiUrl, statsTrophees);
+  }
+
+  // Récupérer toutes les statistiques de statistique de trophées
+  getAllStatsTrophees(): Observable<StatsTrophees[]> {
+    return this.http.get<StatsTrophees[]>(`${this.apiUrl}/all`);
+  }
+
+  // Récupérer une statistique de statistique de trophées par son ID
+  getStatsTropheesById(id: number): Observable<StatsTrophees> {
+    return this.http.get<StatsTrophees>(`${this.apiUrl}/${id}`);
+  }
+
+  // Mettre à jour une statistique de statistique de trophées
+  updateStatsTrophees(statsTrophees: StatsTrophees): Observable<StatsTrophees> {
+    return this.http.put<StatsTrophees>(`${this.apiUrl}/update`, statsTrophees);
+  }
+
+  // Supprimer une statistique de statistique de trophées par son ID
+  deleteStatsTropheesById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  // Supprimer toutes les statistiques de statistique de trophées
+  deleteAllStatsTrophees(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
