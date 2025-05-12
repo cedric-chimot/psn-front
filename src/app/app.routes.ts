21063729880ef61a121ae761ae72299e93dd34b2
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-12 10:53:03 +0200

    ajout de la route liste de trophées

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListeJeuxComponent } from './pages/liste-jeux/liste-jeux.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ListeJeuxComponent },];
