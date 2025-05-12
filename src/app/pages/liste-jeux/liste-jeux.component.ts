323409db2762d1c9d04e792240d3c51cf8dfd611
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-12 11:06:49 +0200

    ajout du html/css Jeux/Plateforme

--- src/app/pages/liste-jeux/liste-jeux.component.ts
+++ src/app/pages/liste-jeux/liste-jeux.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { JeuxPlateforme } from '../../models/JeuxPlateforme';
+import { JeuxPlateformeService } from '../../services/jeuxPlateforme/jeux-plateforme.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-liste-jeux',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './liste-jeux.component.html',
-  styleUrl: './liste-jeux.component.css'
+  styleUrls: ['./liste-jeux.component.css']
 })
 export class ListeJeuxComponent {
+  jeuxPlateformes: JeuxPlateforme[] = [];
 
+  constructor(private jpService: JeuxPlateformeService) {}
+
+  ngOnInit(): void {
+    this.jpService.getAll().subscribe({
+      next: (data) => {
+        this.jeuxPlateformes = data;
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement', err);
+      }
+    });
+  }
 }
