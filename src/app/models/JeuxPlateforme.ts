b752d2208d48750e31dd4118c47625068a6d151e
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-12 10:56:46 +0200

    correction du model Jeux/Plateforme

--- src/app/models/JeuxPlateforme.ts
+++ src/app/models/JeuxPlateforme.ts
@@ -1,8 +1,11 @@
+import { Jeux } from "./Jeux";
+import { Plateforme } from "./Plateforme";
+
 export class JeuxPlateforme {
   constructor(
     public id: number,
-    public idJeu: number,
-    public idPlateforme: number,
+    public idJeu: Jeux,
+    public idPlateforme: Plateforme,
     public nbPLatine: number,
     public nbOr: number,
     public nbArgent: number,
