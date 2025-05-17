3f01a59c8e1c2b50b8f363d6463a507c9e4ef4ac
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-17 13:38:17 +0200

    modification du model jeux

--- src/app/models/Jeux.ts
+++ src/app/models/Jeux.ts
@@ -1,6 +1,14 @@
+import { Plateforme } from "./Plateforme";
+
 export class Jeux {
   constructor(
     public id: number,
-    public jeu: string
+    public jeu: string,
+    public plateforme: Plateforme,
+    public nbPlatine: number,
+    public nbOr: number,
+    public nbArgent: number,
+    public nbBronze: number,
+    public nbHeures: number,
   ) {}
 }
