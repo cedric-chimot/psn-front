4d5b7eec7bb37924f1f48532af1413d83c92414e
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-19 09:37:37 +0200

    initialisation du composant pagination

--- src/app/components/commons/pagination/pagination.component.ts
+++ src/app/components/commons/pagination/pagination.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css'
 })
 export class PaginationComponent {
+  @Input() currentPage!: number;
+  @Input() totalPages!: number;
+  @Output() pageChange = new EventEmitter<number>();
 
+  // Méthode pour changer de page
+  goToPage(page: number) {
+    this.pageChange.emit(page);
+  }
+
+  // Méthode pour générer les numéros de page
+  get pageNumbers() {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
 }
