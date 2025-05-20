bbb6198254573c0d194493c8d0b1ab131ba4e2f7
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-20 10:12:40 +0200

    ajout des pages pour les stats

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListeJeuxComponent } from './pages/liste-jeux/liste-jeux.component';
 import { JeuxPlateformeFormComponent } from './forms/JeuxPlateformeForm/jeux-plateforme-form/jeux-plateforme-form.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: ListeJeuxComponent },
   { path: 'ajout-form', component: JeuxPlateformeFormComponent },
+  { path: 'stats', component: StatsComponent },
 ];
